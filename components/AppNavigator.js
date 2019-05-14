900018364b36167ac5d92044018716235f17cf3b
Charlie Stanard <louis.stanard@gmail.com>
2019-05-14 06:31:46 -0600

    Adding sample setup for navigating between screens

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -1,9 +1,16 @@
 import { createAppContainer, createStackNavigator } from "react-navigation";
-import Home from '../screens/home';
+import Home from '../screens/Home';
+import AddWorkout from '../screens/AddWorkout';
 
-const AppNavigator = createStackNavigator({
-  Home: { screen: Home },
-});
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+    AddWorkout: { screen: AddWorkout }
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
--- screens/AddWorkout.js
+++ screens/AddWorkout.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-export default class Home extends React.Component {
+export default class AddWorkout extends React.Component {
   static navigationOptions = ({navigation}) => {
     return {
-      title: 'Home'
+      title: 'Add a Workout'
     }
   }
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>This is the Home screen.</Text>
-        <Button
-          title="Add a Workout"
-          onPress={() => this.props.navigation.navigate('AddWorkout')} />
+        <Text>Add a New Workout</Text>
+        <Button 
+          title="Add Another Workout"
+          onPress={() => this.props.navigation.push('AddWorkout')} />
       </View>
     );
   }
