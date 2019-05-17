49aa2ae0f6ac37f77048edd99f8f7038fb90b098
Charlie Stanard <louis.stanard@gmail.com>
2019-05-17 08:05:50 -0600

    Adding redux-thunk middleware for async action creators, adding redux-logger middleware

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger'
 import rootReducer from './reducers';
 import AppNavigator from './components/AppNavigator';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
 
 export default class App extends React.Component {
   render() {
