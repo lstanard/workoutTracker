689e116030badb69db09ea81d60d20b739fd72a9
Charlie Stanard <louis.stanard@gmail.com>
2019-05-24 16:49:24 -0600

    Fixing dummy data for workouts

--- reducers/workouts.js
+++ reducers/workouts.js
@@ -71,30 +71,189 @@ const initialState = {
       notes: 'Good energy',
       dateCreated: '2019-05-18',
       exercises: [
-        'Squat',
-        'Bench Press',
-        'Barbell Row'
-      ]
+      {
+        name: 'Bench Press',
+        sets: [{
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Squat',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Barbell Row',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      }]
     },
     {
       title: 'StrongLifts 5x5 B',
       notes: 'Got to gym late',
       dateCreated: '2019-05-16',
       exercises: [
-        'Squat',
-        'Overhead Press',
-        'Deadlift'
-      ]
+      {
+        name: 'Bench Press',
+        sets: [{
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Squat',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Barbell Row',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      }]
     },
     {
       title: 'StrongLifts 5x5 A',
       notes: '',
       dateCreated: '2019-05-14',
       exercises: [
-        'Squat',
-        'Bench Press',
-        'Barbell Row'
-      ]
+      {
+        name: 'Bench Press',
+        sets: [{
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 6,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        },
+        {
+          weight: 135,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Squat',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      },
+      {
+        name: 'Barbell Row',
+        sets: [{
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 6,
+        },
+        {
+          weight: 185,
+          reps: 5,
+        },
+        {
+          weight: 185,
+          reps: 4,
+        }]
+      }]
     },
   ]
 };
